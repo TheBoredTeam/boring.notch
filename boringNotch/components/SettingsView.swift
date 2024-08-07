9c9b2b713facfb0567fe983be75647c686e04a25
Richard Kunkli <richard@kunkli.com>
2024-08-07 19:58:45 +0200

    refactor env object, update music live activity, implement settings and colored spectrogram

---
+++ boringNotch/components/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  boringNotch
+//
+//  Created by Richard Kunkli on 07/08/2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
---
+++ boringNotch/extensions/Image2Color.swift
@@ -0,0 +1,18 @@
+//
+//  Image2Color.swift
+//  boringNotch
+//
+//  Created by Richard Kunkli on 07/08/2024.
+//
+
+import SwiftUI
+
+struct Image2Color: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Image2Color()
+}
