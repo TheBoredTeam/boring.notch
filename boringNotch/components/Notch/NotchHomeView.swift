c8dc21f4b383f30f9257329bb79f25466146d711
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2024-11-06 19:25:22 -0500

    Prevent crashes due to non-finite duration

--- boringNotch/components/Notch/NotchHomeView.swift
+++ boringNotch/components/Notch/NotchHomeView.swift
@@ -220,6 +220,9 @@ struct MusicSliderView: View {
     }
     
     func timeString(from seconds: Double) -> String {
+        if seconds.isNaN || seconds.isInfinite {
+            return "--:--"
+        }
         let totalMinutes = Int(seconds) / 60
         let remainingSeconds = Int(seconds) % 60
         let hours = totalMinutes / 60
