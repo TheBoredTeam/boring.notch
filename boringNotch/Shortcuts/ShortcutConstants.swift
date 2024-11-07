3a9034425355d0316c75ba89dc4bcc098eea8e26
Friedrich Stoltzfus <friedrichstoltzfus@gmail.com>
2024-11-07 13:09:33 -0500

    added shortcut ability to toggle notch open

--- boringNotch/Shortcuts/ShortcutConstants.swift
+++ boringNotch/Shortcuts/ShortcutConstants.swift
@@ -14,4 +14,5 @@ extension KeyboardShortcuts.Name {
     static let decreaseBacklight = Self("decreaseBacklight", default: .init(.f1, modifiers: [.command]))
     static let increaseBacklight = Self("increaseBacklight", default: .init(.f2, modifiers: [.command]))
     static let toggleSneakPeek = Self("toggleSneakPeek", default: .init(.h, modifiers: [.command, .shift]))
+    static let toggleNotchOpen = Self("toggleNotchOpen", default: .init(.i, modifiers: [.command, .shift]))
 }
