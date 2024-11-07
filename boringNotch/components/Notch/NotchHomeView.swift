a81e0948e1aebda0c51dc7463b4097b15b055591
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2024-11-06 21:37:37 -0500

    More fixes

--- boringNotch/components/Notch/NotchHomeView.swift
+++ boringNotch/components/Notch/NotchHomeView.swift
@@ -134,8 +134,7 @@ struct NotchHomeView: View {
     }
     
     private func updateSliderValue() {
-        guard !dragging, musicManager.timestampDate > lastDragged,
-              musicManager.timestampDate > musicManager.lastUpdated else { return }
+        guard !dragging, musicManager.timestampDate > lastDragged else { return }
         let currentTime = Date()
         let timeDifference = (musicManager.isPlaying ? currentTime.timeIntervalSince(musicManager.timestampDate) : musicManager.lastUpdated.timeIntervalSince(musicManager.timestampDate))
         // Calculate the real-time elapsed time
