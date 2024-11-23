88460e2438b3eee9df08a646860d9116c158a6a8
Om Chachad <omchachad12@gmail.com>
2024-11-23 21:35:25 +0530

    Added Move Transition and Enhanced Blur Effect

--- boringNotch/components/Notch/NotchHomeView.swift
+++ boringNotch/components/Notch/NotchHomeView.swift
@@ -24,7 +24,7 @@ struct NotchHomeView: View {
     
     var body: some View {
         Group {
-            if !vm.firstLaunch {
+            if !vm.firstLaunch && vm.notchState == .open {
                 HStack(alignment: .top, spacing: 30) {
                     HStack {
                         ZStack(alignment: .bottomTrailing) {
@@ -178,7 +178,9 @@ struct NotchHomeView: View {
             }
             .transition(.opacity.combined(with: .blurReplace))
         }
-        .blur(radius: vm.notchState == .closed ? 15 : 0)
+        .animation(.smooth.speed(1.5), value: vm.notchState)
+        .transition(.move(edge: .top).combined(with: .blurReplace))
+        .blur(radius: vm.notchState == .closed ? 30 : 0)
     }
     
     private func startTimer() {
