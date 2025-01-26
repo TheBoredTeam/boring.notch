ac088682a8272e1d1d2871c2e63940574a34e8f3
iamharshdev <me@iamharsh.dev>
2025-01-26 10:55:45 +0530

    🔧 chore(Constants): Update release name to "Wolf Painting 🐺🎨"
    - Modified the default release name in Constants.swift
    - Updated the release name to reflect a new creative theme

--- boringNotch/models/Constants.swift
+++ boringNotch/models/Constants.swift
@@ -40,7 +40,7 @@ extension Defaults.Keys {
         // MARK: General
     static let menubarIcon = Key<Bool>("menubarIcon", default: true)
     static let showOnAllDisplays = Key<Bool>("showOnAllDisplays", default: false)
-    static let releaseName = Key<String>("releaseName", default: "Jellyfin Snoring 🪼 (Dreaming)")
+    static let releaseName = Key<String>("releaseName", default: "Wolf Painting 🐺🎨")
     
         // MARK: Behavior
     static let minimumHoverDuration = Key<TimeInterval>("minimumHoverDuration", default: 0.3)
