ac6227308ce9542343a4a44e20cee4de10ea8c87
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2025-04-14 12:09:01 -0400

    Fix frame issues

--- boringNotch/ContentView.swift
+++ boringNotch/ContentView.swift
@@ -299,7 +299,7 @@ struct ContentView: View {
                       }
                   }
               }
-              .conditionalModifier((coordinator.sneakPeek.show && (coordinator.sneakPeek.type == .music) && vm.notchState == .closed && !vm.hideOnClosed) || (coordinator.sneakPeek.show && (coordinator.sneakPeek.type != .music) && (musicManager.isPlaying || !musicManager.isPlayerIdle))) { view in
+              .conditionalModifier((coordinator.sneakPeek.show && (coordinator.sneakPeek.type == .music) && vm.notchState == .closed && !vm.hideOnClosed) || (coordinator.sneakPeek.show && (coordinator.sneakPeek.type != .music) && vm.notchState == .closed)) { view in
                   view
                       .fixedSize()
               }
