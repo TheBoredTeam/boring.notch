d775e7b7c78652146847027059c16a95f1d437df
Andrew Zhao <andrewzhao@Andrews-MacBook-Pro-2.local>
2025-04-21 01:19:36 -0400

    Add open on tap to shelf

--- boringNotch/components/Settings/SettingsView.swift
+++ boringNotch/components/Settings/SettingsView.swift
@@ -611,11 +611,16 @@ struct About: View {
 }
 
 struct Shelf: View {
+    
+    @Default(.shelfTapToOpen) var shelfTapToOpen: Bool
+    
     var body: some View {
         Form {
             Section {
                 Defaults.Toggle("Enable shelf", key: .boringShelf)
                 Defaults.Toggle("Open shelf tab by default if items added", key: .openShelfByDefault)
+                Defaults.Toggle("Open files from shelf", key: .shelfTapToOpen)
+                
             } header: {
                 HStack {
                     Text("General")
