953a1291ce77a994695900454cd161c98bd50fd5
Alexander <33609792+Alexander5015@users.noreply.github.com>
2025-07-26 00:21:28 -0400

    Update version number and name

--- boringNotch/models/Constants.swift
+++ boringNotch/models/Constants.swift
@@ -64,7 +64,7 @@ extension Defaults.Keys {
     static let menubarIcon = Key<Bool>("menubarIcon", default: true)
     static let showOnAllDisplays = Key<Bool>("showOnAllDisplays", default: false)
     static let automaticallySwitchDisplay = Key<Bool>("automaticallySwitchDisplay", default: true)
-    static let releaseName = Key<String>("releaseName", default: "Wolf Painting 🐺🎨")
+    static let releaseName = Key<String>("releaseName", default: "Flying Rabbit 🐇🪽")
     
         // MARK: Behavior
     static let minimumHoverDuration = Key<TimeInterval>("minimumHoverDuration", default: 0.3)
