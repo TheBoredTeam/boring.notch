a1110db8d0c6dd5e42754ce7b50756d9e3d659d1
Unblockerfire <145611531+Unblockerfire@users.noreply.github.com>
2025-11-05 19:01:09 -0700

    Initial Commit

---
+++ Zenith Loft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zenith Loft
+//
+//  Created by Carson Livezey on 11/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
