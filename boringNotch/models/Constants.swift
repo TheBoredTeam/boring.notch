8e1d573b7103df321e2231fc292e3d31bbd33eec
Alexander <33609792+Alexander5015@users.noreply.github.com>
2025-11-24 10:57:02 -0500

    Add option to hide windows on non-notch displays from Mission Control

--- boringNotch/models/Constants.swift
+++ boringNotch/models/Constants.swift
@@ -188,6 +188,7 @@ extension Defaults.Keys {
     static let customAccentColorData = Key<Data?>("customAccentColorData", default: nil)
     // Show or hide the title bar
     static let hideTitleBar = Key<Bool>("hideTitleBar", default: true)
+    static let hideNonNotchedFromMissionControl = Key<Bool>("hideNonNotchedFromMissionControl", default: true)
     
     // Helper to determine the default media controller based on NowPlaying deprecation status
     static var defaultMediaController: MediaControllerType {
