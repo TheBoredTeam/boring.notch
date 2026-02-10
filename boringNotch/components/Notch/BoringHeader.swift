d3a0ee32147512414dbc10fb869b29e88d3e6a3f
Jay-D13 <jaydaninparis@gmail.com>
2026-02-10 17:57:50 -0500

    added notch microphone mute button

--- boringNotch/components/Notch/BoringHeader.swift
+++ boringNotch/components/Notch/BoringHeader.swift
@@ -12,6 +12,7 @@ struct BoringHeader: View {
     @EnvironmentObject var vm: BoringViewModel
     @ObservedObject var batteryModel = BatteryStatusViewModel.shared
     @ObservedObject var coordinator = BoringViewCoordinator.shared
+    @ObservedObject var microphoneManager = MicrophoneManager.shared
     @StateObject var tvm = ShelfStateViewModel.shared
     var body: some View {
         HStack(spacing: 0) {
@@ -58,6 +59,22 @@ struct BoringHeader: View {
                             }
                             .buttonStyle(PlainButtonStyle())
                         }
+                        Button(action: {
+                            Task { @MainActor in
+                                MicrophoneManager.shared.toggleMuteAction()
+                            }
+                        }) {
+                            Capsule()
+                                .fill(.black)
+                                .frame(width: 30, height: 30)
+                                .overlay {
+                                    Image(systemName: microphoneManager.isMuted ? "mic.slash" : "mic")
+                                        .foregroundColor(microphoneManager.isMuted ? .red : .white)
+                                        .padding()
+                                        .imageScale(.medium)
+                                }
+                        }
+                        .buttonStyle(PlainButtonStyle())
                         if Defaults[.settingsIconInNotch] {
                             Button(action: {
                                 SettingsWindowController.shared.showWindow()
