a623cc6875c6f485bc485af251b07d30ec5a7f1c
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2026-02-26 13:54:24 +0800

    Fix notch content alignment

--- boringNotch/ContentView.swift
+++ boringNotch/ContentView.swift
@@ -233,6 +233,7 @@ struct ContentView: View {
         }
         .padding(.bottom, 8)
         .frame(maxWidth: windowSize.width, maxHeight: windowSize.height, alignment: .top)
+        .ignoresSafeArea(.all)
         .compositingGroup()
         .scaleEffect(
             x: gestureScale,
