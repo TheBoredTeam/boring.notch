8eb9d8256c8f17ab7902cc6cd16ce81d394deb1b
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2026-04-15 15:55:44 -0400

    Add FFT Waveform visual

--- boringNotch/models/Constants.swift
+++ boringNotch/models/Constants.swift
@@ -190,6 +190,7 @@ extension Defaults.Keys {
     
     // MARK: Media playback
     static let coloredSpectrogram = Key<Bool>("coloredSpectrogram", default: true)
+    static let realtimeAudioWaveform = Key<Bool>("realtimeAudioWaveform", default: false)
     static let enableSneakPeek = Key<Bool>("enableSneakPeek", default: false)
     static let sneakPeekStyles = Key<SneakPeekStyle>("sneakPeekStyles", default: .standard)
     static let waitInterval = Key<Double>("waitInterval", default: 3)
