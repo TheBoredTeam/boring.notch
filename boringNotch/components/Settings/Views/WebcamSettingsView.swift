040932e7865114beffdc45e07110fccf0de12208
Alexander <33609792+Alexander5015@users.noreply.github.com>
2026-09-16 12:59:25 -0600

    fix: remove accidental webcam settings text

--- boringNotch/components/Settings/Views/WebcamSettingsView.swift
+++ boringNotch/components/Settings/Views/WebcamSettingsView.swift
@@ -36,7 +36,7 @@ struct WebcamSettingsView: View {
                         .tag(nil as String?)
                     ForEach(camera.availableCameras) { camera in
                         Text(camera.name)
-he                             .tag(camera.id as String?)
+                            .tag(camera.id as String?)
                     }
                 }
                 .disabled(!showMirror || !checkVideoInput())
