aa26a07bc95fb7712ec837962326322a1090e889
Alexander <33609792+Alexander5015@users.noreply.github.com>
2026-09-23 00:48:20 -0600

    Change default release name to 'Dapper Crab 🎩🦀'

--- boringNotch/models/Constants.swift
+++ boringNotch/models/Constants.swift
@@ -387,7 +387,7 @@ extension Defaults.Keys {
     static let menubarIcon = Key<Bool>("menubarIcon", default: true)
     static let showOnAllDisplays = Key<Bool>("showOnAllDisplays", default: false)
     static let automaticallySwitchDisplay = Key<Bool>("automaticallySwitchDisplay", default: true)
-    static let releaseName = Key<String>("releaseName", default: "Flying Rabbit 🐇🪽")
+    static let releaseName = Key<String>("releaseName", default: "Dapper Crab 🎩🦀")
     static let updateChannel = Key<UpdateChannel>("updateChannel", default: UpdateChannel.bundled)
 
     // MARK: Behavior
